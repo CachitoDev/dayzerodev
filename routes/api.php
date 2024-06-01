dce152f82f7b0366064c9dac04267c41a7bc547d
Mauricio Flores Hernandez <mau1361317@gmail.com>
2024-06-01 11:17:11 -0600

    chore: fix paginate

--- routes/api.php
+++ routes/api.php
@@ -26,8 +26,8 @@ Route::get('logs', function (Request $request) {
 
     $id = $request->input('id');
     if (is_numeric($id)) {
-        return \App\Models\RequestLog::query()->where('id', $id)->paginate(10);
+        return \App\Models\RequestLog::query()->where('id', $id)->paginate(5);
     }
 
-    return \App\Models\RequestLog::query()->paginate(50);
+    return \App\Models\RequestLog::query()->paginate(5);
 });
